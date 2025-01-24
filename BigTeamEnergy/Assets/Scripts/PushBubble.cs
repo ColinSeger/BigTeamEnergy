c2ceaf2944a8f1fe1a053898bd610f603fa9ec61
ColinSeger <colin.segerljung.cs@gmail.com>
2025-01-24 22:18:19 +0100

    Bug fix

--- BigTeamEnergy/Assets/Scripts/PushBubble.cs
+++ BigTeamEnergy/Assets/Scripts/PushBubble.cs
@@ -1,30 +1,28 @@
 using UnityEngine;
-
+public enum Direction :byte{
+    Left,
+    Right
+}
 public class PushBubble : MonoBehaviour
 {
-    enum Direction :byte{
-        Left,
-        Right
-    }
+    
     [SerializeField] float pushForce = 10f;
     [SerializeField] Direction direction = Direction.Left;
     void OnTriggerStay(Collider collision){
         var bubble = collision.gameObject.GetComponent<Rigidbody>();
         if(bubble){
-            Debug.Log("This should not");
             bubble.AddForce(this.transform.forward * pushForce * Time.deltaTime);
         }
     }
     void OnTriggerStay2D(Collider2D collider){
         var bubble = collider.gameObject.GetComponent<Rigidbody2D>();
-        Debug.Log("das");
         if(bubble){
             float speed = 2;
             if(direction == Direction.Left){
                 speed = -2;
             }
-            bubble.AddForce(speed * bubble.transform.right);
-            bubble.MoveRotation(Vector2.Angle(collider.transform.position, this.transform.position));
+            bubble.AddForce(speed * pushForce * bubble.transform.right);
+            bubble.SetRotation(Vector2.Angle(collider.gameObject.transform.position, this.transform.position));
         }
     }
 }
