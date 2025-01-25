518b72f3d79aaf8f2e2c955f9b3ff807484a07f1
Peperyjam <peperyjam051@hotmail.com>
2025-01-25 18:43:15 +0100

    Added Working Main Menu, credits and options not implemented

---
+++ BigTeamEnergy/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void OptionsButton()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void CreditsButton()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void Exitutton()
+    {
+        Application.Quit();
+    }
+}
