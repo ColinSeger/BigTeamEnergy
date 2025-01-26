0c37217d94ae3ce18eac07a64e73b84b02495b56
ColinSeger <colin.segerljung.cs@gmail.com>
2025-01-26 12:15:32 +0100

    Animation fix

--- BigTeamEnergy/Assets/Scripts/Player1Controll.cs
+++ BigTeamEnergy/Assets/Scripts/Player1Controll.cs
@@ -13,19 +13,26 @@ public class Player1Controll : MonoBehaviour
     public float minInterval = 2f;   // Minimum delay between sounds
     public float maxInterval = 5f;   // Maximum delay between sounds
     [Range(0f, 1f)] public float playChance = 0.5f;  // Chance to play sound (0.0 to 1.0)
+    bool isMoving = false;
+    [SerializeField] Animator animator;
     void FixedUpdate(){
         var up = Physics2D.Raycast(this.transform.position ,Vector2.up, distance);
         var down = Physics2D.Raycast(this.transform.position ,Vector2.down, distance);
         if(Input.GetKey(KeyCode.W) && !up){
             this.transform.position += new Vector3(0, 1, 0) * speed * Time.fixedDeltaTime;
+            isMoving = true;
             StartCoroutine(PlaySoundWithRandomChance());
             StopCoroutine(PlaySoundWithRandomChance());
         }
         else if(Input.GetKey(KeyCode.S) && !down){
             this.transform.position -= new Vector3(0, 1, 0) * speed * Time.fixedDeltaTime;
+            isMoving = true;
             StartCoroutine(PlaySoundWithRandomChance());
             StopCoroutine(PlaySoundWithRandomChance());
-        } 
+        }else{
+            isMoving = false;
+        }
+        animator.SetBool("isMoving", isMoving);
     }
 
     private IEnumerator PlaySoundWithRandomChance()
