05d278377fb7445bc8e3e9688ca41bb970e4755b
bensimonschelhaas <98400638+bensimonschelhaas@users.noreply.github.com>
2025-01-26 12:44:01 +0100

    volume slider

--- BigTeamEnergy/Assets/Scripts/VolumeSlider.cs
+++ BigTeamEnergy/Assets/Scripts/VolumeSlider.cs
@@ -1,16 +1,75 @@
 using UnityEngine;
+using UnityEngine.UI;
+using FMODUnity;
+using FMOD.Studio;
 
 public class VolumeSlider : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+
+
+    [Header("UI Sliders")]
+    [SerializeField] private Slider masterVolumeSlider;
+    [SerializeField] private Slider sfxVolumeSlider;
+    [SerializeField] private Slider musicVolumeSlider;
+
+    [Header("FMOD VCA Names")]
+    [SerializeField] private string masterVCAPath = "vca:/MasterVCA";
+    [SerializeField] private string sfxVCAPath = "vca:/SFXVCA";
+    [SerializeField] private string musicVCAPath = "vca:/MusicVCA";
+
+    private VCA masterVCA;
+    private VCA sfxVCA;
+    private VCA musicVCA;
+
+    private void Start()
     {
-        
+        // Get VCA references
+        masterVCA = RuntimeManager.GetVCA(masterVCAPath);
+        sfxVCA = RuntimeManager.GetVCA(sfxVCAPath);
+        musicVCA = RuntimeManager.GetVCA(musicVCAPath);
+
+        // Load saved volume settings or set defaults
+        masterVolumeSlider.value = PlayerPrefs.GetFloat("MasterVolume", 1f);
+        sfxVolumeSlider.value = PlayerPrefs.GetFloat("SFXVolume", 1f);
+        musicVolumeSlider.value = PlayerPrefs.GetFloat("MusicVolume", 1f);
+
+        // Apply volume settings on start
+        SetMasterVolume(masterVolumeSlider.value);
+        SetSFXVolume(sfxVolumeSlider.value);
+        SetMusicVolume(musicVolumeSlider.value);
+
+        // Add listeners for when sliders are changed
+        masterVolumeSlider.onValueChanged.AddListener(SetMasterVolume);
+        sfxVolumeSlider.onValueChanged.AddListener(SetSFXVolume);
+        musicVolumeSlider.onValueChanged.AddListener(SetMusicVolume);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetMasterVolume(float value)
     {
-        
+        masterVCA.setVolume(value);
+        PlayerPrefs.SetFloat("MasterVolume", value);  // Save volume
     }
+
+    public void SetSFXVolume(float value)
+    {
+        sfxVCA.setVolume(value);
+        PlayerPrefs.SetFloat("SFXVolume", value);
+    }
+
+    public void SetMusicVolume(float value)
+    {
+        musicVCA.setVolume(value);
+        PlayerPrefs.SetFloat("MusicVolume", value);
+    }
+
+    private void OnDestroy()
+    {
+        // Remove listeners when the object is destroyed
+        masterVolumeSlider.onValueChanged.RemoveListener(SetMasterVolume);
+        sfxVolumeSlider.onValueChanged.RemoveListener(SetSFXVolume);
+        musicVolumeSlider.onValueChanged.RemoveListener(SetMusicVolume);
+    }
+
+
 }
